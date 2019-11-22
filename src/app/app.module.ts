5b787d549e1f121c6119d0839e1502540321eef1
greegor1 <grzegorz27393@gmail.com>
2019-11-22 16:23:35 +0100

    playing with reactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
@@ -15,7 +16,8 @@ import { CommonModule } from '@angular/common';
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
